497802d08003dfb72caec76903bbb228e1b876bf
Pavankumar Nalla <pavankumarnalla.official@gmail.com>
2026-07-30 16:48:56 +0530

    Time: 46 ms (13.81%), Space: 47.1 MB (59.01%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -5,7 +5,7 @@ class Solution {
         boolean flag = false;
 
         for(i=0; i< nums.length; i++){
-            for(j=0; j<nums.length; j++){
+            for(j=i+1; j<nums.length; j++){
                 if(i == j)
                 continue;
 
