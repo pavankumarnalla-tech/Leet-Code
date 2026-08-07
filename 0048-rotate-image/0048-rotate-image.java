e783636c283b0340781be1c58d6853736eda8ea5
Pavankumar Nalla <pavankumarnalla.official@gmail.com>
2026-08-07 22:32:00 +0530

    Time: 0 ms (100%), Space: 43.6 MB (87.06%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,7 +1,9 @@
 class Solution {
     public void rotate(int[][] matrix) {
+
         for(int i=0; i<matrix.length; i++){
             for(int j=i+1; j<matrix.length; j++){
+
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
@@ -9,6 +11,7 @@ class Solution {
         }
 
         for(int i = 0; i<matrix.length; i++){
+            
             int left = 0;
             int right = matrix.length-1;
 
