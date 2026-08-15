46d5ef9f0b01875cf67a6c1b5593a0b97c950aa7
Pavankumar Nalla <pavankumarnalla.official@gmail.com>
2026-08-15 23:53:36 +0530

    Time: 46 ms (36.49%), Space: 64.5 MB (41.04%) - LeetHub

---
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -0,0 +1,59 @@
+class Solution {
+
+    public int reversePairs(int[] nums) {
+        return mergeSort(nums, 0, nums.length - 1);
+    }
+
+    public int mergeSort(int[] nums, int low, int high) {
+
+        if (low >= high)
+            return 0;
+
+        int mid = low + (high - low) / 2;
+
+        int count = 0;
+
+        count += mergeSort(nums, low, mid);
+        count += mergeSort(nums, mid + 1, high);
+
+        int j = mid + 1;
+
+        for (int i = low; i <= mid; i++) {
+
+            while (j <= high && (long) nums[i] > 2L * nums[j])
+                j++;
+
+            count += j - (mid + 1);
+        }
+
+        merge(nums, low, mid, high);
+
+        return count;
+    }
+
+    public void merge(int[] nums, int low, int mid, int high) {
+
+        int[] temp = new int[high - low + 1];
+
+        int i = low;
+        int j = mid + 1;
+        int k = 0;
+
+        while (i <= mid && j <= high) {
+
+            if (nums[i] <= nums[j])
+                temp[k++] = nums[i++];
+            else
+                temp[k++] = nums[j++];
+        }
+
+        while (i <= mid)
+            temp[k++] = nums[i++];
+
+        while (j <= high)
+            temp[k++] = nums[j++];
+
+        for (i = low; i <= high; i++)
+            nums[i] = temp[i - low];
+    }
+}
