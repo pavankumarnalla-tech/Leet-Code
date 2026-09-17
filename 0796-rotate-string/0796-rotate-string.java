c7ba865bf046357c7d7c435a0f19df4be507b178
Pavankumar Nalla <pavankumarnalla.official@gmail.com>
2026-09-17 22:16:34 +0530

    Time: 1 ms (98.48%), Space: 43.5 MB (49.97%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        
+        if(s.length() == goal.length()){
+            if((s+s).contains(goal))
+            return true;
+        }
+
+        return false;
+    }
+}
